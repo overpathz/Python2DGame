35e3ada1dda67e5199a148a7258aa8fb2910a8c9
overpath <overpath@gmail.com>
2020-12-04 15:37:27 +0200

    Added: background song

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ pg.init()
 
 W, H = 1280, 720
 sc = pg.display.set_mode((W, H))
-pg.display.set_caption('MADE BY KLYMENKO & KUTSELA')
+pg.display.set_caption('MADE BY KLYMENKO & (А КУЦЕЛА ШО? СПИТЬ НАХУЙ ПРОСПАВ ВСІ ПАРИ)')
 pg.display.set_icon(pg.image.load('logo/logo.png'))
 bg = pg.image.load("sprites/bg.png")
 ground = pg.image.load("sprites/ground.png")
+idle = pg.image.load('sprites/idle.png')
+
+pg.mixer.music.load('sounds/bgMountains1.mp3')
+pg.mixer.music.set_volume(0.1)
+pg.mixer.music.play(-1)
 
 running = True
 
@@ -19,6 +24,7 @@ height = 40
 
 isJump = False
 isJumping = False
+standing = False
 jumpCount = 10
 
 walkRight = [pg.image.load('sprites/heroRight.png'), pg.image.load('sprites/heroRight2.png'), pg.image.load('sprites/heroRight.png'), pg.image.load('sprites/heroRight2.png'), pg.image.load('sprites/heroRight.png'), pg.image.load('sprites/heroRight2.png'), pg.image.load('sprites/heroRight.png'), pg.image.load('sprites/heroRight2.png')]
@@ -66,6 +72,10 @@ def drawWindow():
         player.body = jumpUp
         sc.blit(player.body[animCount // 5], (x, y))
         animCount += 1
+    elif standing:
+        player.body = idle
+        sc.blit(player.body, (x, y))
+        animCount += 1
 
     pg.display.update()
 
@@ -88,7 +98,8 @@ while running:
         right = True
     else:
         left = False
-        right = True
+        right = False
+        standing = True
         animCount = 0
 
     if not isJump:
@@ -101,9 +112,10 @@ while running:
         if jumpCount >= -10:
             if jumpCount < 0:
                 y += (jumpCount ** 2) / 2
-                isJumping = True
+                isJumping = False
                 left = False
                 right = False
+                standing = True
             else:
                 y -= (jumpCount ** 2) / 2
                 isJumping = True
